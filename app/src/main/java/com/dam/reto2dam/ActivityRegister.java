70d3dec85c90fb8931e6b389f18571cf643334a3
Carlos <carlosrafael-mg@hotmail.com>
2019-10-30 14:15:51 +0100

    Register is halfway done

--- app/src/main/java/com/dam/reto2dam/ActivityRegister.java
+++ app/src/main/java/com/dam/reto2dam/ActivityRegister.java
@@ -1,11 +1,17 @@
 package com.dam.reto2dam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class ActivityRegister extends AppCompatActivity {
+import thebestprogramlogiclibrary.User;
+import thebestprogramlogiclibrary.logic.ApplicationLogicImplementation;
+
+public class ActivityRegister extends Activity implements View.OnClickListener {
     private Button btnSubmit;
     private Button btnCancel;
     private EditText fieldUsername;
@@ -13,12 +19,29 @@ public class ActivityRegister extends AppCompatActivity {
     private EditText fieldConfirmPassword;
     private EditText fieldEmail;
     private EditText fieldFullName;
+    private User user;
+    private ApplicationLogicImplementation appLogic;
+
+    public void setAppLogic(ApplicationLogicImplementation appLogic) {
+        this.appLogic = appLogic;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) extends Activity implements OnClickListener{
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        findViews();
+
+        btnSubmit.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+        btnSubmit.setEnabled(false);
+
+        // how to add Listeners to the fields?
+    }
+
+    private void findViews() {
         btnSubmit = findViewById(R.id.btnSubmit);
         btnCancel = findViewById(R.id.btnCancel);
         fieldUsername = findViewById(R.id.fieldUsername);
@@ -26,21 +49,32 @@ public class ActivityRegister extends AppCompatActivity {
         fieldConfirmPassword = findViewById(R.id.fieldConfirmPassword);
         fieldEmail = findViewById(R.id.fieldEmail);
         fieldFullName = findViewById(R.id.fieldFullName);
-
-        btnSubmit.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
-
-
     }
-    public void onClick(View v){
+
+    /**
+     * This method dictates what happens when the buttons are pressed
+     *
+     * @param v
+     */
+    public void onClick(View v) {
         try {
-            if (v.getId == btnSubmit.getId) {
-
+            if (v.getId() == btnSubmit.getId()) {
+                user = new User();
+                user.setEmail(String.valueOf(fieldEmail.getText()));
+                user.setFullName(String.valueOf(fieldFullName.getText()));
+                user.setLogin(String.valueOf(fieldUsername.getText()));
+                user.setPassword(String.valueOf(fieldPassword.getText()));
+                if (appLogic.registerUser(user)) {
+                    Intent mainActivity = new Intent(activity_aplicationmainmenu.xml);
+                    //open the intent somehow
+                }
             } else {
-
+                //How to go back to the previous activity (login)
             }
         } catch (Exception e) {
-
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage(e.getMessage());
+            alert.show();
         }
 
     }
