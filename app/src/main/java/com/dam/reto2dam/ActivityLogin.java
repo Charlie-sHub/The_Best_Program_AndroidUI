ec7ce08812b9baf57050406c58aeea28cf71d81e
ander <arodriguezi2000@gmail.com>
2019-11-04 12:52:19 +0100

    Activity login

--- app/src/main/java/com/dam/reto2dam/ActivityLogin.java
+++ app/src/main/java/com/dam/reto2dam/ActivityLogin.java
@@ -1,12 +1,15 @@
 package com.dam.reto2dam;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.ArrayList;
 
-public class    ActivityLogin extends AppCompatActivity {
+public class    ActivityLogin extends AppCompatActivity implements View.OnClickListener {
     private TextView txtTitle;
     private TextView txtUsername;
     private TextView txtPassword;
@@ -15,14 +18,46 @@ public class    ActivityLogin extends AppCompatActivity {
     private Button btnLogin;
     private TextView txtNoAccount;
     private Button btnRegister;
+    private ArrayList<EditText> textFields;
+    private User user;
+    private ApplicationLogicImplementation appLogic;
+
+    public void setAppLogic(ApplicationLogicImplementation appLogic) {
+        this.appLogic = appLogic;
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViews();
+
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+
+        textFields = new ArrayList<EditText>();
+
+        addFieldsToArray();
     }
 
+
+
+    private void findViews() {
+        txtTitle = findViewById(R.id.txtTitle);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        fieldUsername = findViewById(R.id.fieldUsername);
+        fieldPassword = findViewById(R.id.fieldPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtNoAccount = findViewById(R.id.txtNoAccount);
+        btnRegister = findViewById(R.id.btnRegister);
+    }
+    private void addFieldsToArray() {
+        textFields.add(fieldUsername);
+        textFields.add(fieldPassword);
+
+    }
     public void onBtnLoginPress(){
 
     }
@@ -33,5 +68,34 @@ public class    ActivityLogin extends AppCompatActivity {
 
     public void onBtnRegisterPress() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+            if (v.getId() == btnLogin.getId()){
+                boolean filledFields = true;
+                for (EditText field : textFields) {
+                    if (field.getText().length() == 0) {
+                        filledFields = false;
+                        break;
+                    }
+                }
+                if(filledFields){
+
+
+                }
+            }
+
+        }catch (Exception e){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage(e.getMessage());
+            alert.show();
+        }
+    }
+    private void userLogin(){
+        user = new User();
+        user.setName = fieldUsername;
+        user.setPassword = fieldPassword;
     }
 }
