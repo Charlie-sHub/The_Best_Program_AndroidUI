8a977ff89dd2bea3af54c4d6537d8ee414d6fae0
ander <arodriguezi2000@gmail.com>
2019-11-04 12:07:35 +0100

    Activity login

--- app/src/main/java/com/dam/reto2dam/ActivityLogin.java
+++ app/src/main/java/com/dam/reto2dam/ActivityLogin.java
@@ -2,12 +2,36 @@ package com.dam.reto2dam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ActivityLogin extends AppCompatActivity {
+public class    ActivityLogin extends AppCompatActivity {
+    private TextView txtTitle;
+    private TextView txtUsername;
+    private TextView txtPassword;
+    private EditText fieldUsername;
+    private EditText fieldPassword;
+    private Button btnLogin;
+    private TextView txtNoAccount;
+    private Button btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onBtnLoginPress(){
+
+    }
+
+    public void onCheckedHappyMode() {
+
+    }
+
+    public void onBtnRegisterPress() {
+
     }
 }
