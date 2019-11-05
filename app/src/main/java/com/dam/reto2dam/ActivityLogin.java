74c75d9a2cce857692e1f72b0d62b9def6046be3
ander <arodriguezi2000@gmail.com>
2019-11-05 10:04:17 +0100

    Activity login finished

--- app/src/main/java/com/dam/reto2dam/ActivityLogin.java
+++ app/src/main/java/com/dam/reto2dam/ActivityLogin.java
@@ -1,34 +1,26 @@
 package com.dam.reto2dam;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import java.util.ArrayList;
 import thebestprogramlogiclibrary.User;
 import thebestprogramlogiclibrary.logic.ApplicationLogicImplementation;
 
 
 public class    ActivityLogin extends AppCompatActivity implements View.OnClickListener {
-    private TextView txtTitle;
-    private TextView txtUsername;
-    private TextView txtPassword;
+
     private EditText fieldUsername;
     private EditText fieldPassword;
     private Button btnLogin;
-    private TextView txtNoAccount;
     private Button btnRegister;
     private ArrayList<EditText> textFields;
     private User user;
     private ApplicationLogicImplementation appLogic;
-
-    public void setAppLogic(ApplicationLogicImplementation appLogic) {
-        this.appLogic = appLogic;
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +39,9 @@ public class    ActivityLogin extends AppCompatActivity implements View.OnClickL
 
 
     private void findViews() {
-        txtTitle = findViewById(R.id.txtTitle);
-        txtUsername = findViewById(R.id.txtUsername);
-        txtPassword = findViewById(R.id.txtPassword);
         fieldUsername = findViewById(R.id.fieldUsername);
         fieldPassword = findViewById(R.id.fieldPassword);
         btnLogin = findViewById(R.id.btnLogin);
-        txtNoAccount = findViewById(R.id.txtNoAccount);
         btnRegister = findViewById(R.id.btnRegister);
     }
     private void addFieldsToArray() {
@@ -61,49 +49,14 @@ public class    ActivityLogin extends AppCompatActivity implements View.OnClickL
         textFields.add(fieldPassword);
 
     }
-    public void onBtnLoginPress(){
-
-    }
-
-    public void onCheckedHappyMode() {
-
-    }
-
-    public void onBtnRegisterPress() {
-
-    }
 
     @Override
     public void onClick(View v) {
         try {
             if (v.getId() == btnLogin.getId()){
-                boolean filledFields = true;
-                for (EditText field : textFields) {
-                    if (field.getText().length() == 0) {
-                        filledFields = false;
-                        break;
-                    }
-                }
-                if(filledFields){
-                    userLogin();
-                    ClientThread client = new ClientThread();
-                    client.setAction("LOGIN");//Cambia a setAction() el mensaje
-                    client.setUser(user);
-                    client.setAppLogic(appLogic);
-                    client.start();
-                    try {
-                        client.join();
-                    }catch (Exception e){
-                        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                        alert.setMessage(e.getMessage());
-                        alert.show();
-                    }
-
-                }else{
-                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                    alert.setMessage("There are empty fields");
-                    alert.show();
-                }
+                onBtnLoginPress();
+            }else if (v.getId() == btnRegister.getId()){
+                onBtnRegisterPress();
             }
 
         }catch (Exception e){
@@ -116,5 +69,60 @@ public class    ActivityLogin extends AppCompatActivity implements View.OnClickL
         user = new User();
         user.setLogin(String.valueOf(fieldUsername.getText()));
         user.setPassword(String.valueOf(fieldPassword.getText()));
+    }
+
+    /**
+     * This method will be on charge of doing the login.
+     * Will be initialised when the client click the login method
+     * Firstly will verify if all the fields are filled, then would try to connect and verify the data
+     */
+    public void onBtnLoginPress(){
+        boolean filledFields = true;
+        for (EditText field : textFields) {
+            if (field.getText().length() == 0) {
+                filledFields = false;
+                break;
+            }
+        }
+        if(filledFields){
+            userLogin();
+            ClientThread client = new ClientThread();
+            client.setAction("LOGIN");//Cambiar a setAction() el mensaje
+            client.setUser(user);
+            client.setAppLogic(appLogic);
+            client.start();
+            try {
+                client.join();
+            }catch (Exception e){
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage(e.getMessage());
+                alert.show();
+            }
+            if (client.getMessage().getContent() instanceof User){
+                user = (User) client.getMessage().getContent();
+                Intent androidMainActivityIntent = new Intent(this, ActivityApplicationMainMenu.class);
+                androidMainActivityIntent.putExtra("user", user);
+                startActivity(androidMainActivityIntent);
+            }else {
+                Exception e = (Exception) client.getMessage().getContent();
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setMessage(e.getMessage());
+                alert.show();
+            }
+
+        }else{
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage("There are empty fields");
+            alert.show();
+        }
+    }
+
+    /**
+     * This function will be on charge of throwing the user to the register windows
+     * Will be initialised when the user clicks on the register button
+     */
+    public void onBtnRegisterPress() {
+        Intent registerIntent = new Intent(this, ActivityRegister.class);
+        startActivity(registerIntent);
     }
 }
